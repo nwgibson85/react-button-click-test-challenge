2bb06a5716609fc7a57f7804e3aa1adad40c5d29
Nicholas Gibson <Nwgibson85@gmail.com>
2019-11-05 07:47:01 -0600

    for the lols

--- src/components/button.test.js
+++ src/components/button.test.js
@@ -4,6 +4,7 @@ import {shallow} from 'enzyme';
 import Button from './button';
 
 describe('<Button />', () => {
+    console.log('hello');
     it('Should call props.onClick when the button is clicked', () => {
         const wrapper = shallow(<Button />);
         wrapper.find('button').simulate('click');
